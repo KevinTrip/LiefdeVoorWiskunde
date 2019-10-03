18cf29c064eed9a4909312f89f6898db50550c2c
KevinTrip <43602032+KevinTrip@users.noreply.github.com>
2019-10-03 17:24:21 +0200

    Update index.php

--- - Liefde voor wiskunde/Lvwsite28_3/index.php
+++ - Liefde voor wiskunde/Lvwsite28_3/index.php
@@ -1,95 +1,96 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<head>
-    <!--
+<html>
+	<head>
+		<!--
 
-    Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
-    -->
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
-    <title></title>
+		Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
+		-->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<title></title>
 
-    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-    <!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
-    <!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+		<!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
+		<!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
 
-    <script type="text/javascript" src="script.js"></script>
-</head>
-<body>
-    <div id="art-page-background-gradient"></div>
-    <div id="art-main">
-        <div class="art-sheet">
-            <div class="art-sheet-tl"></div>
-            <div class="art-sheet-tr"></div>
-            <div class="art-sheet-bl"></div>
-            <div class="art-sheet-br"></div>
-            <div class="art-sheet-tc"></div>
-            <div class="art-sheet-bc"></div>
-            <div class="art-sheet-cl"></div>
-            <div class="art-sheet-cr"></div>
-            <div class="art-sheet-cc"></div>
-            <div class="art-sheet-body">
-				<?php
-					include("includes/header.php")
-				?>
-                <div class="art-content-layout">
-                    <div class="art-content-layout-row">
-                        <div class="art-layout-cell art-content">
-                            <div class="art-post">
-                                <div class="art-post-body">
-									<div class="art-post">
-										<div class="art-post-body">
-											<div class="art-post-inner art-article">
-												<div class="art-postmetadataheader">
-													<h2 class="art-postheader">
-														Welkom voor een ieder die geïntereseerd is in Wiskunde
-													</h2>
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+		<div id="art-page-background-gradient"></div>
+		<div id="art-main">
+			<div class="art-sheet">
+				<div class="art-sheet-tl"></div>
+				<div class="art-sheet-tr"></div>
+				<div class="art-sheet-bl"></div>
+				<div class="art-sheet-br"></div>
+				<div class="art-sheet-tc"></div>
+				<div class="art-sheet-bc"></div>
+				<div class="art-sheet-cl"></div>
+				<div class="art-sheet-cr"></div>
+				<div class="art-sheet-cc"></div>
+				<div class="art-sheet-body">
+					<?php
+						include("includes/header.php")
+					?>
+					<div class="art-content-layout">
+						<div class="art-content-layout-row">
+							<div class="art-layout-cell art-content">
+								<div class="art-post">
+									<div class="art-post-body">
+										<div class="art-post">
+											<div class="art-post-body">
+												<div class="art-post-inner art-article">
+													<div class="art-postmetadataheader">
+														<h2 class="art-postheader">
+															Welkom voor een ieder die geïntereseerd is in Wiskunde
+														</h2>
+													</div>
+													<div class="art-postcontent">
+														<!-- article-content -->
+														<p> Sommige zien wiskunde als een vervelend onderwerp op school, maar andere halen hun hele wereld eruit op.</p>
+														<p>Op deze site kun je verschillende informatie en uitleg over wiskudige onderwerpen vinden die voor iedereen beschikbaar is.</p>                                              
+													</div>
+													<div class="art-postmetadataheader">
+														<h2 class="art-postheader">
+															Achtergrond informatie
+														</h2>
+													</div>
+													<div class="art-postcontent">
+														<!-- article-content -->
+														<p>De informatie te lezen op de site is gemaakt door en voor mensen die werkzaam zijn in of met wiskunde.</p>
+														<p>Dit verschilt tussen spefieke onderwerpen, tot gobale lesinformatie waar je gebruik van kan maken.</p>
+														<br><br>
+														<p>  Hier wordt niet gezocht naar een methode om de liefde te vangen met wiskunde, zeg maar de wiskunde voor de liefde, maar hier willen we de liefde voor wiskunde bereiken.</p> 
+														<p>Hier zijn we op zoek naar de oorsprong.</p>
+														<p>Daar waar de leerling geraakt wordt, waar een kiem wordt gezaaid voor nieuwe wiskundigen, nieuwe getallengekken en formulebouwers.</p>
+														<br><br>
+														<p> Mocht je zelf een inbreng doen aan de website, dan staan we daar voor open.</p>
+														<span class="art-button-wrapper">
+															<span class="l"> </span>
+															<span class="r"> </span>
+															<a class="art-button" href="javascript:void(0)">Werk mee!</a>
+														</span>
+														<p>Ontwerp website: V. Fokkema, S. Musters, K. Trip</p>
+													<!-- /article-content -->
+													</div>
+													<div class="cleared"></div>
 												</div>
-												<div class="art-postcontent">
-													<!-- article-content -->
-													<p> Sommige zien wiskunde als een vervelend onderwerp op school, maar andere halen hun hele wereld eruit op.
-													<p>Op deze site kun je verschillende informatie en uitleg over wiskudige onderwerpen vinden die voor iedereen beschikbaar is.</p>                                              
-												</div>
-												<div class="art-postmetadataheader">
-													<h2 class="art-postheader">
-														Achtergrond informatie
-													</h2>
-												</div>
-												<div class="art-postcontent">
-													<!-- article-content -->
-													<p>De informatie te lezen op de site is gemaakt door en voor mensen die werkzaam zijn in of met wiskunde.</p>
-													<p>Dit verschilt tussen spefieke onderwerpen, tot gobale lesinformatie waar je gebruik van kan maken.</p>
-													<br><br>
-													<p>  Hier wordt niet gezocht naar een methode om de liefde te vangen met wiskunde, zeg maar de wiskunde voor de liefde, maar hier willen we de liefde voor wiskunde bereiken. 
-													<p>Hier zijn we op zoek naar de oorsprong.
-													<p>Daar waar de leerling geraakt wordt, waar een kiem wordt gezaaid voor nieuwe wiskundigen, nieuwe getallengekken en formulebouwers.</p>
-													<br><br>
-													<p> Mocht je zelf een inbreng doen aan de website, dan staan we daar voor open.</p>
-													<span class="art-button-wrapper">
-                                                		<span class="l"> </span>
-                                                		<span class="r"> </span>
-                                                		<a class="art-button" href="javascript:void(0)">Werk mee!</a>
-                                                	</span>
-                                                	<p>Ontwerp website: V. Fokkema, S. Musters, K. Trip</p>
-                                                <!-- /article-content -->
-												</div>
-												<div class="cleared"></div>
+											<div class="cleared"></div>
 											</div>
-										<div class="cleared"></div>
 										</div>
 									</div>
+									<div class="cleared"></div>
 								</div>
-                                <div class="cleared"></div>
-                            </div>
-                        </div>
-						<div class="cleared"></div>
-                    </div>
-                </div>
-                <div class="cleared"></div>
-            </div>
-        </div>
-    </div>
-	<?php
-		include("includes/footer.php");
-	?>
-</body>
+							</div>
+							<div class="cleared"></div>
+						</div>
+					</div>
+					<div class="cleared"></div>
+				</div>
+			</div>
+		</div>
+		<?php
+			include("includes/footer.php");
+		?>
+	</body>
 </html>
