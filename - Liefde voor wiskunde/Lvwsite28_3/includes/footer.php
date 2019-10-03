5d556229cb4a1a7c297450c14a2fd961f4e17f72
KevinTrip <43602032+KevinTrip@users.noreply.github.com>
2019-10-03 11:48:17 +0200

    update footer.php
    Ingedent bij de footer.php

--- - Liefde voor wiskunde/Lvwsite28_3/includes/footer.php
+++ - Liefde voor wiskunde/Lvwsite28_3/includes/footer.php
@@ -1,17 +1,22 @@
-<div class="cleared"></div><div class="art-footer">
-                    <div class="art-footer-inner">
-
-                        <div class="art-footer-text">
-                            <p><a class="footer-list" href="contact.php">Contact Us</a> | <a href="">Terms of Use</a> | <a href="#">Trademarks</a>
-                                | <a class="footer-list" href="#">Privacy Statement</a><br />
-                                Copyright &copy; <?php $year = getdate();
-								echo $year["year"]; ?> ---. All Rights Reserved.</p>
-                        </div>
-                    </div>
-                    <div class="art-footer-background"></div>
-                </div>
-        		<div class="cleared"></div>
+<div class="cleared"></div>
+	<div class="art-footer">
+		<div class="art-footer-inner">
+            <div class="art-footer-text">
+            <p>
+				<a class="footer-list" href="contact.php">Contact Us</a> | <a href="">Terms of Use</a> | <a href="#">Trademarks</a>
+                | <a class="footer-list" href="#">Privacy Statement</a><br />
+                Copyright &copy; 
+				<?php 
+					$year = getdate();
+					echo $year["year"]; 
+				?> 
+				---. All Rights Reserved.</p>
             </div>
         </div>
-        <div class="cleared"></div>
-        <p class="art-page-footer">Atelier 2019</p>
+        <div class="art-footer-background"></div>
+    </div>
+    <div class="cleared"></div>
+</div>
+</div>
+<div class="cleared"></div>
+<p class="art-page-footer">Atelier 2019</p>
