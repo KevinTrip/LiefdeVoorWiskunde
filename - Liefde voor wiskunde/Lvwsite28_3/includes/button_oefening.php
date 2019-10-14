9fed6c9aee791f5dbe96539f47be41847ec2eba2
KevinTrip <43602032+KevinTrip@users.noreply.github.com>
2019-10-14 11:43:38 +0200

    button_oefening.php created
    De button_oefening.php gemaakt zodat hij geinclude kan worden in andere pagina's

---
+++ - Liefde voor wiskunde/Lvwsite28_3/includes/button_oefening.php
@@ -0,0 +1,7 @@
+<p>
+	<span class="art-button-wrapper">
+		<span class="l"> </span>
+		<span class="r"> </span>
+		<a class="art-button" href="extra_oefeningen.php">Extra oefeningen</a>
+	</span>                                              	
+</p>  
